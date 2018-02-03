c4020a00f75e164737a4995ec7f0eb6024b1ceb3
Naren Kasinath <narenkasinath@outlook.com>
2018-02-03 11:54:57 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team540.robot;
 
 import com.mindsensors.CANSD540;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -10,177 +12,252 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-    final String baselineAuto = "Baseline";
-    final String switchAuto = "Switch";
-    String autoSelected;
-    SendableChooser<String> chooser = new SendableChooser<>();
+	final String defaultAuto = "Default";
+	final String baselineSide = "Baseline (S)";
+	final String baselineMiddle = "Baseline (M)";
+	final String switchLeft = "Switch (L)";
+	final String switchMiddle = "Switch (M)";
+	final String switchRight = "Switch (R)";
 
-    //motor, joy-stick, and controller fields
-    CANSD540 frontLeft, frontRight, backLeft, backRight, midLeft, midRight, intake1, intake2, intakeVert;
-    Joystick leftJoy, rightJoy;
-    XboxController xbox;
-    
-    //sensor fields
-    Encoder encoder1;
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
 
-    //will be used in driveCode() to get current movement
-    double yLeft, yRight;
+	// motor, joy-stick, and controller fields
+	CANSD540 frontLeft, frontRight, backLeft, backRight, midLeft, midRight, intake1, intake2, intakeVert, hook, winch;
+	Joystick leftJoy, rightJoy;
+	XboxController xbox;
 
-    @Override
-    public void robotInit() {
-        chooser.addDefault("Baseline", baselineAuto);
-        chooser.addObject("Switch", switchAuto);
-        SmartDashboard.putData("Auto choices", chooser);
+	// Sensor fields
+	Encoder enc1;
+	ADXRS450_Gyro gyro;
+	AnalogInput IR;
 
-        //wheel motors
-        frontLeft = new CANSD540(6);
-        midLeft = new CANSD540(7);
-        backLeft = new CANSD540(8);
-        frontRight = new CANSD540(3);
-        midRight = new CANSD540(4);
-        backRight = new CANSD540(5);
+	// will be used in driveCode() to get current movement
+	double yLeft, yRight, intakeL, intakeR, hooker, wench;
 
-        //intake motors
-        intake1 = new CANSD540(9);
-        intake2 = new CANSD540(10);
-        intakeVert = new CANSD540(11);
+	// sensor fields
+	double angle, irDist, pulse, dist;
 
-        //joystick and controller motors
-        leftJoy = new Joystick(0);
-        rightJoy = new Joystick(1);
-        xbox = new XboxController(0);
+	@Override
+	public void robotInit() {
+		chooser.addDefault("Default (Do Nothing)", defaultAuto);
+		chooser.addObject("Baseline (From Side Position)", baselineSide);
+		chooser.addObject("Baseline (From Middle Position)", baselineMiddle);
+		chooser.addObject("Switch (From Left Position)", switchLeft);
+		chooser.addObject("Switch (From Middle Position)", switchMiddle);
+		chooser.addObject("Switch (From Right Position)", switchRight);
 
-        //encoders
-        encoder1 = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-        encoder1.setMaxPeriod(0.1);
-        encoder1.setMinRate(5);
-        encoder1.setDistancePerPulse(4);
-        encoder1.setSamplesToAverage(10);
-        
-        //initialize voltage ramps      
-        frontLeft.setVoltageRamp(100);
-        midLeft.setVoltageRamp(100);
-        backLeft.setVoltageRamp(100);
-        frontRight.setVoltageRamp(100);
-        midRight.setVoltageRamp(100);
-        backRight.setVoltageRamp(100);
-      
-        intake1.setVoltageRamp(100);
-        intake2.setVoltageRamp(100);
-        intakeVert.setVoltageRamp(100);
-    }
+		SmartDashboard.putData("Auto choices", chooser);
 
-    public void autonomousInit() {
-        System.out.println("Auto selected: " + autoSelected);
-    }
+		// wheel motors
+		frontLeft = new CANSD540(6);
+		midLeft = new CANSD540(7);
+		backLeft = new CANSD540(8);
+		frontRight = new CANSD540(3);
+		midRight = new CANSD540(4);
+		backRight = new CANSD540(5);
 
-    @Override
-    public void autonomousPeriodic() {
-        switch (autoSelected) {
-        case switchAuto:
-            //Put custom auto code here
-            break;
-        case baselineAuto:
+		// intake motors
+		intake1 = new CANSD540(9);
+		intake2 = new CANSD540(10);
+		intakeVert = new CANSD540(11);
 
-            //Move forward 10ft across baseline
-            //Encoders determine distance
+		// climber and winch
+		hook = new CANSD540(12);
+		winch = new CANSD540(13);
 
-            //TODO: actually test the encoder values - needs to go 10 ft+
-            if (encoder1.get() <= 10) {
-                frontRight.set(1);
-                backRight.set(1);
-                frontLeft.set(1);
-                backLeft.set(1);
-            } else {
-                frontRight.set(0);
-                backRight.set(0);
-                frontLeft.set(0);
-                backLeft.set(0);
-            }
+		// joystick and controller motors
+		leftJoy = new Joystick(0);
+		rightJoy = new Joystick(1);
+		xbox = new XboxController(0);
 
-            break;
-        default:
+		// encoders
+		enc1 = new Encoder(0, 1);
+		enc1.setMaxPeriod(0.1);
+		enc1.setMinRate(5);
+		enc1.setDistancePerPulse(4);
+		enc1.setSamplesToAverage(10);
 
-        }
-    }
+		// sensors
+		gyro = new ADXRS450_Gyro();
+		IR = new AnalogInput(0);
 
-    @Override
-    public void teleopPeriodic() {
-        //calls driveCode() to drive
-        driveCode();
-        
-        //calls intake() to deal with cube manipulation
-        intake();        
-    }
-    
-    //drive code
-    private void driveCode() {
-		//drive code
-    	
-    	//set current positions of joysticks as values to yLeft and yRight
-        yLeft = -leftJoy.getY();
-        yRight = -rightJoy.getY();
+		// calibrates sensors
+		gyro.reset();
+		gyro.calibrate();
+		enc1.reset();
 
-    	// TODO: 0.5 is the dead-zone - fine tune this to the bot
-        if (Math.abs(yLeft) < 0.1) {
-            frontLeft.set(0);
-            midLeft.set(0);
-            backLeft.set(0);
-        } else {
-            frontLeft.set(-yLeft);
-            midLeft.set(-yLeft);
-            backLeft.set(-yLeft);
-        }
+		// initialize voltage ramps
+		frontLeft.setVoltageRamp(100);
+		midLeft.setVoltageRamp(100);
+		backLeft.setVoltageRamp(100);
+		frontRight.setVoltageRamp(100);
+		midRight.setVoltageRamp(100);
+		backRight.setVoltageRamp(100);
 
-        if (Math.abs(yRight) < 0.1) {
-            frontRight.set(0);
-            midRight.set(0);
-            backRight.set(0);
-        } else {
-            frontRight.set(yRight);
-            midRight.set(yRight);
-            backRight.set(yRight);
-        }
+		intake1.setVoltageRamp(100);
+		intake2.setVoltageRamp(100);
+		intakeVert.setVoltageRamp(100);
 	}
 
-    //intake code
-	public void intake() {
-    	//intake in
-        if(xbox.getRawAxis(2) < 0.7) {
-        	intake1.set(0);
-        	intake2.set(0);
-        }
-        else {
-        	intake1.set(xbox.getRawAxis(2));
-        	intake2.set(xbox.getRawAxis(2));
-        }
-        //intake out
-        if(xbox.getRawAxis(3) < 0.7) {
-        	intake1.set(0);
-        	intake2.set(0);
-        }
-        else {
-        	intake1.set(xbox.getRawAxis(3));
-        	intake2.set(xbox.getRawAxis(3));
-        }
-        
-        //vert go up
-        if(xbox.getXButton() == true) {
-        	intakeVert.set(.5);
-        }
-        else {
-        	intakeVert.set(0);
-        }
-        
-        //vert go down
-        if(xbox.getAButton() == true) {
-        	intake1.set(-0.7);
-        	intake2.set(-0.7);
-        }
-        else {
-        	intake1.set(0);
-        	intake2.set(0);
-        }
-    }
-}
+	public void autonomousInit() {
+		System.out.println("Auto selected: " + autoSelected);
+	}
 
+	@Override
+	public void autonomousPeriodic() {
+		// Gets sensor values and displays them in SmartDashboard
+		angle = gyro.getAngle();
+		irDist = IR.getVoltage();
+		pulse = enc1.get();
+		dist = enc1.getDistance();
+
+		SmartDashboard.putNumber("Angle: ", angle);
+		SmartDashboard.putNumber("IR Distance: ", irDist);
+		SmartDashboard.putNumber("Pulse Count: ", pulse);
+		SmartDashboard.putNumber("Distance Traveled: ", dist);
+
+		switch (autoSelected) {
+
+		case baselineSide:
+
+			break;
+
+		case baselineMiddle:
+
+			break;
+
+		case switchLeft:
+
+			break;
+
+		case switchMiddle:
+
+			break;
+
+		case switchRight:
+
+			break;
+
+		case defaultAuto:
+			motorSet(0, 0);
+			break;
+		}
+	}
+
+	public static double prop(double target, double x) {
+		return (target - x) / target;
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		// gets sensor values and displays them in SmartDashboard
+		angle = gyro.getAngle();
+		irDist = IR.getVoltage();
+		pulse = enc1.get();
+		dist = enc1.getDistance();
+
+		SmartDashboard.putNumber("Angle: ", angle);
+		SmartDashboard.putNumber("IR Distance: ", irDist);
+		SmartDashboard.putNumber("Pulse Count: ", pulse);
+		SmartDashboard.putNumber("Distance Traveled: ", dist);
+
+		// calls drive() to drive
+		drive();
+
+		// calls intake() to deal with cube manipulation
+		intake();
+
+		// calls switch() to raise the intake to switch height
+		switcher();
+
+		// calls climb() to deploy hook and pull in the winch
+		climb();
+	}
+
+	// drive code
+	private void drive() {
+		// drive code
+
+		// set current positions of joysticks as values to yLeft and yRight
+		if (Math.abs(leftJoy.getY()) > 0.1) {
+			yLeft = leftJoy.getY();
+		} else {
+			yLeft = 0;
+		}
+		if (Math.abs(rightJoy.getY()) > 0.1) {
+			yRight = rightJoy.getY();
+		} else {
+			yRight = 0;
+		}
+
+		motorSet(yLeft, yRight);
+	}
+
+	// sets drivetrain motor speed
+	private void motorSet(double x, double y) {
+		frontLeft.set(-x);
+		midLeft.set(-x);
+		backLeft.set(-x);
+
+		frontRight.set(-y);
+		midRight.set(-y);
+		backRight.set(-y);
+	}
+
+	// intake code
+	private void intake() {
+		if (xbox.getRawAxis(1) > 0.7) {
+			intakeL = xbox.getRawAxis(1);
+			intakeR = xbox.getRawAxis(1);
+		} else {
+			intakeL = 0;
+			intakeR = 0;
+		}
+
+		intakeMotorSet(intakeL, intakeR);
+	}
+
+	// raises and lowers intake for switch
+	private void switcher() {
+		// Goes up
+		if (xbox.getXButton() == true) {
+			intakeVert.set(.5);
+		} else {
+			intakeVert.set(0);
+		}
+
+		// Goes down
+		if (xbox.getAButton() == true) {
+			intake1.set(-0.5);
+			intake2.set(-0.5);
+		} else {
+			intake1.set(0);
+			intake2.set(0);
+		}
+	}
+
+	// sets intake motor speed
+	private void intakeMotorSet(double x, double y) {
+		intake1.set(x);
+		intake2.set(y);
+	}
+
+	// sets hook & winch motor speed
+	private void climb() {
+		if (xbox.getRawAxis(5) > 0.7) {
+			hooker = xbox.getRawAxis(5);
+		} else {
+			hooker = 0;
+		}
+		if (xbox.getRawAxis(3) > 0.7) {
+			wench = xbox.getRawAxis(3);
+		} else {
+			wench = 0;
+		}
+
+		hook.set(hooker);
+		winch.set(wench);
+	}
+
+}
